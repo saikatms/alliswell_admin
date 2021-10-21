47c718aef2afd32cf1be8db3296d76ffc88a8d73
Saikat Mondal <saikatm1997@gmail.com>
2021-10-21 14:39:05 +0530

    Update firebase.js

--- all_is_well_admin/src/firebase.js
+++ all_is_well_admin/src/firebase.js
@@ -11,14 +11,14 @@ import "firebase/auth";
 import "firebase/firestore";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAX50Nc-IEQ3C1nl584-QKGwQIOZkUTv0M",
-  authDomain: "alliswell-99d18.firebaseapp.com",
-  databaseURL: "https://alliswell-99d18-default-rtdb.firebaseio.com",
-  projectId: "alliswell-99d18",
-  storageBucket: "alliswell-99d18.appspot.com",
-  messagingSenderId: "551951265424",
-  appId: "1:551951265424:web:04d71e802c95067ab57215",
-  measurementId: "G-2W12DJ4HYV",
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 firebase.initializeApp(firebaseConfig);
 
